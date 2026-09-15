82e72877a1ed0b421eacf911caabb12a222e88bb
mhmed200620061975-stack <mhmed200620061975@gmail.com>
2026-09-15 16:46:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Project Settings -> Data API
 window.DP_SUPABASE_URL = "https://ekyokkbtpgebptzuwplm.supabase.co";
 // استخدم Publishable/Anon key فقط في الواجهة، وليس Service Role key.
-window.DP_SUPABASE_ANON_KEY = "sb_publishable_ZILmnskAONN-gZjsGYsOxA_WSnTHqnY";
+window.DP_SUPABASE_ANON_KEY = "sb_publishable_ZILmnskA0NN-gZjsGYs0xA_WSnTHqnY";
